eb2daa20c2244dd3709da1c34f702c05bc85fab6
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-21 18:26:04 -0400

    Add style to the main container

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
 import AddEntry from './components/AddEntry';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+});
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar />
       <AddEntry />
     </View>
