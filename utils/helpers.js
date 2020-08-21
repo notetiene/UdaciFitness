e9aefd0738c99e2b2fd9dadf9bd4fba54da48350
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-21 17:11:34 -0400

    Fix bad icon name

--- utils/helpers.js
+++ utils/helpers.js
@@ -61,7 +61,7 @@ export function getMetricMetaInfo(metric) {
         return (
           <View>
             <MaterialIcons
-              name="direction-run"
+              name="directions-run"
               color="black"
               size={35}
             />
