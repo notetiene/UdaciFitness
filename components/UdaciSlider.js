05e123811e881cef1e1441290b91a7ba95a87fc0
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-21 18:26:25 -0400

    Add Slider to the UdaciSlider component
    * components/UdaciSlider.js: Add the Slider.
    * utils/common.js: Add prop-types definition.

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Slider, Text } from 'react-native';
+import { UdaciSliderPropType } from '../utils/common';
 
-export default function UdaciSlider() {
+export default function UdaciSlider({
+  max,
+  unit,
+  step,
+  value,
+  onChange,
+}) {
   return (
     <View>
-      <Text>UdaciSlider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 }
+
+UdaciSlider.propTypes = UdaciSliderPropType;
--- utils/common.js
+++ utils/common.js
@@ -1,3 +1,10 @@
 import PropTypes from 'prop-types';
 
 export const datePropType = PropTypes.string;
+export const UdaciSliderPropType = PropTypes.exact({
+  max: PropTypes.number,
+  unit: PropTypes.string,
+  step: PropTypes.number,
+  value: PropTypes.number,
+  onChange: PropTypes.func,
+}).isRequired;
