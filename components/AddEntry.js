1ae457e78cebdeec2d6f01d44f3ad74a6b1b023d
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-25 18:30:20 -0400

    Fix icon name

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -58,7 +58,7 @@ export default function AddEntry({ alreadyLogged }) {
     return (
       <View>
         <Ionicons
-          name="ios-happy-outline"
+          name="ios-happy"
           size={100}
         />
         <Text>
