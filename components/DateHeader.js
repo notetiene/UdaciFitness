06c0a93ab68c0e545aa9a8f876eab9a209c0205f
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-26 16:55:00 -0400

    Add color to DateHeader

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { datePropType } from '../utils/common';
+import { purple } from '../utils/colors';
 
 export default function DateHeader({ date }) {
   return (
-    <Text>
+    <Text
+      style={{ color: purple, fontSize: 25 }}
+    >
       {date}
     </Text>
   );
