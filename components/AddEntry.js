0d3c0f3fd26bde3473a9774f22109b023d66ee31
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-26 14:43:51 -0400

    Add styles to addEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,8 @@ import {
   View,
   TouchableOpacity,
   Text,
+  Platform,
+  StyleSheet,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -21,6 +23,51 @@ import {
   removeEntry,
 } from '../utils/api';
 import { addEntry } from '../actions';
+import { white, purple } from '../utils/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosSubmitButton: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  androidSubmitButton: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  }
+});
 
 const defaultState = {
   run: 0,
@@ -33,9 +80,14 @@ const defaultState = {
 function SubmitButton({ onPress }) {
   return (
     <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosSubmitButton : styles.androidSubmitButton}
       onPress={onPress}
     >
-      <Text>Submit</Text>
+      <Text
+        style={styles.submitBtnText}
+      >
+        Submit
+      </Text>
     </TouchableOpacity>
   );
 }
@@ -61,15 +113,20 @@ function AddEntry({ alreadyLogged, doAddEntry }) {
 
   if (alreadyLogged) {
     return (
-      <View>
+      <View
+        style={styles.center}
+      >
         <Ionicons
-          name="ios-happy"
+          name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
           size={100}
         />
         <Text>
           You already logged your information for today
         </Text>
-        <TextButton onPress={reset}>
+        <TextButton
+          style={{ padding: 10 }}
+          onPress={reset}
+        >
           Reset
         </TextButton>
       </View>
@@ -123,7 +180,9 @@ function AddEntry({ alreadyLogged, doAddEntry }) {
   const metaInfo = getMetricMetaInfo();
 
   return (
-    <View>
+    <View
+      style={styles.container}
+    >
       <DateHeader
         date={(new Date()).toLocaleDateString()}
       />
@@ -139,7 +198,10 @@ function AddEntry({ alreadyLogged, doAddEntry }) {
         const value = state[key];
 
         return (
-          <View key={key}>
+          <View
+            style={styles.row}
+            key={key}
+          >
             {getIcon()}
             {type === 'slider'
               ? (
--- utils/helpers.js
+++ utils/helpers.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     padding: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 100,
+    borderRadius: 8,
     marginRight: 20,
   },
 });
