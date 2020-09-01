da6b0e63516ec6bf65b4c7f3b247420d9c2db6f0
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-09-01 17:21:45 -0400

    Add UdaciFitnessCalendar component
    * components/UdaciFitnessCalendar.js: Since the course, a lot has
    changed in the requested library and react native.  This is a wrapper
    to add a compatibility layer.

---
+++ components/UdaciFitnessCalendar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Calendar } from 'react-native-calendars';
+import { timeToString } from '../utils/helpers';
+
+export default function UdaciFitnessCalendar({
+  items,
+  renderItem,
+  renderEmptyDate,
+}) {
+  const [selectedDate, setSelectedDate] = useState(timeToString());
+  const dates = Object.entries(items);
+
+  const markedDates = Object.fromEntries(dates.map(([key, value]) => [key, {
+    marked: value !== null,
+    selected: key === selectedDate,
+  }]));
+  const handleDateChange = ({ dateString }) => {
+    setSelectedDate(dateString);
+  };
+
+  return (
+    <View>
+      <Calendar
+        markedDates={markedDates}
+        maxDate={timeToString()}
+        onDayPress={handleDateChange}
+        enableSwipeMonths
+      />
+      {(items[selectedDate] && renderItem(items[selectedDate]))
+       || renderEmptyDate(items[selectedDate])}
+    </View>
+  );
+}
+
+UdaciFitnessCalendar.propTypes = {
+  items: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.exact({
+      bike: PropTypes.number,
+      eat: PropTypes.number,
+      run: PropTypes.number,
+      sleep: PropTypes.number,
+      swim: PropTypes.number,
+    }),
+    PropTypes.exact({
+      today: PropTypes.string,
+    }),
+    PropTypes.oneOf([null]),
+  ])).isRequired,
+  renderItem: PropTypes.func.isRequired,
+  renderEmptyDate: PropTypes.func.isRequired,
+};
