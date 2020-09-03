6b0cebe28ccfc21aed0f51bfc16f55ad3d5287ea
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-09-03 14:47:35 -0400

    Add Navigation to App.js

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, { Fragment } from 'react';
 import {
   View,
+  StatusBar,
   StyleSheet,
+  Platform,
 } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry';
 import reducer from './reducers';
+import History from './components/History';
+import { purple, white } from './utils/colors';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,12 +27,71 @@ const styles = StyleSheet.create({
   },
 });
 
+const HistoryScreen = () => (
+  <View style={styles.container}>
+    <History />
+  </View>
+);
+
+const EntryScreen = () => (
+  <View style={styles.container}>
+    <AddEntry />
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
-        <AddEntry />
-      </View>
-    </Provider>
+    <Fragment>
+      <StatusBar />
+      <Provider store={createStore(reducer)}>
+        <NavigationContainer
+          options={{
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+              height: 56,
+              backgroundColor: Platform.OS === 'ios' ? white : purple,
+              shadowColor: 'rgba(0, 0, 0, 0.24)',
+              shadowOffset: {
+                width: 0,
+                height: 3,
+              },
+              shadowRadius: 6,
+              shadowOpacity: 1,
+            },
+          }}
+        >
+          <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen
+              name="History"
+              component={HistoryScreen}
+              options={{
+                tabBarIcon: ({ tintColor }) => (
+                  <Ionicons
+                    name="ios-bookmarks"
+                    size={30}
+                    color={tintColor}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Add Entry"
+              component={EntryScreen}
+              options={{
+                tabBarIcon: ({ tintColor }) => (
+                  <FontAwesome
+                    name="plus-square"
+                    size={30}
+                    color={tintColor}
+                  />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </Fragment>
   );
 }
